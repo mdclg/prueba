593a702bc06ce1170fda01b6a49b5b9975d66fbf
Mari Carmen Legaz <mdclg3@um.es>
2019-03-23 16:58:33 +0100

    funcion suma

--- src/prog.c
+++ src/prog.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int suma(int a, int b){
+	return a + b;
+}
+
 int main(){
 
-	printf("Haciendo pruebas con git");
+	printf("Haciendo pruebas con git\n");
+	int total = suma(5,3);
+	printf("total %d", total);
+
 
 } 
